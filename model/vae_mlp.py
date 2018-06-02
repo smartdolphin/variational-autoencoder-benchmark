5d6415d963f3c9001d16c72e9c703e89640171be
Tae ho Lee <smartdolphin07@gmail.com>
2018-06-03 00:36:58 +0900

    Change log variance to standard deviation
    we output the log variance instead of the standard deviation
    because this is not only more convenient to work with,
    but also helps with numerical stability.
    
    Signed-off-by: Tae ho Lee <smartdolphin07@gmail.com>

--- model/vae_mlp.py
+++ model/vae_mlp.py
@@ -24,10 +24,10 @@ def sampling(args):
 
     z_mean, z_log_var = args
     batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
+    latent_dim = K.int_shape(z_mean)[1]
     # by default, random_normal has mean=0 and std=1.0
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + z_log_var * epsilon
+    epsilon = K.random_normal(shape=(batch, latent_dim))
+    return z_mean + K.exp(z_log_var * 0.5) * epsilon
 
 
 def vae_mlp(original_dim: int, intermediate_dim: int, latent_dim: int, dropout_rate=0.1):
@@ -45,16 +45,14 @@ def vae_mlp(original_dim: int, intermediate_dim: int, latent_dim: int, dropout_r
     gaussian_params = Dense(latent_dim * 2, name='gaussian_params', kernel_initializer=VarianceScaling())(x)
     # The mean parameter is unconstrained
     mean = Lambda(lambda param: param[:, :latent_dim])(gaussian_params)
-    # The standard deviation must be positive. Parametrize with a softplus and
-    # add a small epsilon for numerical stability
-    stddev = Lambda(lambda param: 1e-6 + K.softplus(param[:, latent_dim:]))(gaussian_params)
+    log_var = Lambda(lambda param: param[:, latent_dim:])(gaussian_params)
 
     # use reparameterization trick to push the sampling out as input
     # note that "output_shape" isn't necessary with the TensorFlow backend
-    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([mean, stddev])
+    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([mean, log_var])
 
     # instantiate encoder model
-    encoder = Model(inputs, [mean, stddev, z], name='encoder')
+    encoder = Model(inputs, [mean, log_var, z], name='encoder')
     encoder.summary()
     plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
 
@@ -80,7 +78,7 @@ def vae_mlp(original_dim: int, intermediate_dim: int, latent_dim: int, dropout_r
 
     # vae loss
     marginal_likelihood = K.sum(inputs * K.log(y) + (1 - inputs) * K.log(1 - y), 1)
-    kl_divergence = K.mean(0.5 * K.sum(K.sqrt(mean) + K.sqrt(stddev) - K.log(1e-8 + K.sqrt(stddev)) - 1, 1))
+    kl_divergence = K.mean(0.5 * K.sum(K.sqrt(mean) + K.sqrt(log_var) - K.log(1e-8 + K.sqrt(log_var)) - 1, 1))
     marginal_likelihood = K.mean(marginal_likelihood)
     elbo = marginal_likelihood - kl_divergence
     vae_loss = -elbo
